a8faf3a4913c2912f7f20cc5e3d24f7306959c58
Bình Nguyên <ngobinhnguyen582000@gmail.com>
2025-06-07 14:41:42 +0700

    Delete 01 Project AUTOSAR directory

--- 01 Project AUTOSAR/00 Project_MCU/ECU/StopEngine.c
+++
@@ -1,6 +0,0 @@
-#include "StopEngine.h"
-#include <stdio.h>
-
-void StopEngine_Run(void) {
-    printf("[StopEngine] Engine stop command received. Shutting down engine.\n");
-}
--- 01 Project AUTOSAR/00 Project_MCU/ECU/StopEngine.h
+++
@@ -1,9 +0,0 @@
-#ifndef STOPENGINE_H
-#define STOPENGINE_H
-
-/**
- * @brief   Logic SWC StopEngine: in log tắt động cơ.
- */
-void StopEngine_Run(void);
-
-#endif /* STOPENGINE_H */
